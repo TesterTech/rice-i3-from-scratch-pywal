6e9dd336b208941d49222faff84ebb9a8960906c
Eric de Graaf <endegraaf@gmail.com>
2022-12-26 16:14:00 +0100

    Add files via upload

---
+++ dot_config/polybar/hack/scripts/launcher.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# this launches drun
+#rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/hack/scripts/rofi/launcher.rasi
+rofi -no-config -no-lazy-grab -show drun -theme ~/.config/rofi/launcher_side.rasi
