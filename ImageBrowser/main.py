893b3300da883303ae265f1e0761574d379077ad
Eric de Graaf <endegraaf@gmail.com>
2023-06-11 21:35:57 +0200

    16 colors

--- ImageBrowser/main.py
+++ ImageBrowser/main.py
@@ -4,9 +4,11 @@
 # uses tkinter to render the gui.
 import os
 import subprocess
-from tkinter import Button, Label, Tk
-
+import time
+from tkinter import Button, Label, Tk, Checkbutton, IntVar
 from PIL import ImageTk, Image
+
+from helpers.TooltipHelper import Tooltip
 
 TEXT_LABEL_FORWARD = " >> "
 TEXT_LABEL_BACK = " << "
@@ -26,6 +28,7 @@ def prep_move(img_no: int):
     global button_back
     global button_exit
     global button_colors
+    global checkbox_16_colors
 
     set_current_image_nr(img_no - 1)
     image_on_grid.grid_forget()
@@ -52,7 +55,7 @@ def forward(image_number: int):
             root, text=TEXT_LABEL_FORWARD, command=lambda: forward(image_number + 1)
         )
     place_buttons_in_grid(
-        button_back, button_exit, button_forward, button_pywal, button_colors
+        button_back, button_exit, button_forward, button_pywal, button_colors, checkbox_16_colors
     )
 
 
@@ -68,27 +71,40 @@ def back(img_number: int):
             root, text=TEXT_LABEL_BACK, command=lambda: back(img_number - 1)
         )
     place_buttons_in_grid(
-        button_back, button_exit, button_forward, button_pywal, button_colors
+        button_back, button_exit, button_forward, button_pywal, button_colors, checkbox_16_colors
     )
 
 
 def run_wal_on_image(img_no: int):
+    """
+    Run the image through the wal command with some arguments.
+    To support 16 colors, a different version is needed, check:
+    https://github.com/eylles/pywal16.git
+    This version supports the  --cols16 argument.
+    A toggle has been built into the GUI to toggle this.
+    """
     filename = List_of_original_images[img_no].filename
-    subprocess.run([f"{HOME}/scripts/pywal.sh", f"{filename}"])
+    print(f'>> Run pywal on {filename} >> {sixteen_colors.get()}')
+    if sixteen_colors.get():
+        subprocess.run([f"{HOME}/scripts/pywal.sh", f"{filename}", "--cols16"])
+    else:
+        subprocess.run([f"{HOME}/scripts/pywal.sh", f"{filename}", "-q"])
 
 
 def place_buttons_in_grid(
-    btn_back: Button,
-    btn_exit: Button,
-    btn_forward: Button,
-    btn_pywal: Button,
-    btn_colors: Button,
+        btn_back: Button,
+        btn_exit: Button,
+        btn_forward: Button,
+        btn_pywal: Button,
+        btn_colors: Button,
+        chk_16_colors: Checkbutton,
 ):
     btn_forward.grid(row=1, column=0)
     btn_back.grid(row=1, column=1)
     btn_pywal.grid(row=1, column=2)
-    btn_exit.grid(row=1, column=3)
+    chk_16_colors.grid(row=1, column=3)
     btn_colors.grid(row=1, column=4)
+    btn_exit.grid(row=1, column=5)
 
 
 def add_images_to_list():
@@ -112,7 +128,7 @@ def check_if_svg(file_name: str) -> bool:
 
 
 def scale_and_crop_images(
-    List_of_images: list, List_of_original_images: list, res: list
+        List_of_images: list, List_of_original_images: list, res: list
 ):
     for image_name in res:
         # print(f'found {image_name} in wall dir. ')
@@ -174,11 +190,13 @@ def get_colors_from_xrdb() -> tuple:
 if __name__ == "__main__":
     root = Tk()
     root.tk.call(
-        "tk", "scaling", 2.0
+        "tk", "scaling", 1.0
     )  # https://www.tcl.tk/man/tcl8.6/TkCmd/tk.html#M10
     root.title("Pywal Image Browser")
     root.geometry("1200x700")
     root.config(bg="lightgrey", pady=20, padx=20)
+    global sixteen_colors
+    sixteen_colors = IntVar()
 
     add_images_to_list()
     image_on_grid = Label(text="Pywal Image Browser", height=15, width=50)
@@ -187,9 +205,12 @@ if __name__ == "__main__":
     button_exit = Button(root, text="Exit", command=root.quit)
     button_forward = Button(root, text=TEXT_LABEL_FORWARD, command=lambda: forward(1))
     button_pywal = Button(root, text="Pywal", command=lambda: run_wal_on_image(img_no))
+    checkbox_16_colors = Checkbutton(root, text='16 colors', variable=sixteen_colors)
+    Tooltip(checkbox_16_colors, text='*Important*\nOnly works with Pywal (fork) supporting 16 colors!',
+            wraplength=200)
     button_colors = Button(root, text="get colors", command=lambda: color_button_grid())
     place_buttons_in_grid(
-        button_back, button_exit, button_forward, button_pywal, button_colors
+        button_back, button_exit, button_forward, button_pywal, button_colors, checkbox_16_colors
     )
-
     root.mainloop()
+
